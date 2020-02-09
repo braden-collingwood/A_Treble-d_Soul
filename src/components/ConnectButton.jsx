0761fec0ffc738aef197861c9ee8ea7e377216f9
vundervaffle <wuwenhao@gmail.com>
2020-02-08 19:11:09 -0500

    button code

---
+++ src/components/ConnectButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+import { green, purple } from '@material-ui/core/colors';
+
+export default function ConnectButton() {
+    return (
+        
+     <Button variant = "contained" color = "secondary">Connect</Button>
+       
+    )
+}
--- src/components/SampleComponent.jsx
+++ src/components/SampleComponent.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
+import { green, purple } from '@material-ui/core/colors';
+import ConnectButton from './ConnectButton';
 
 export default function SampleComponent() {
     return (
         <div>
-            
+        <ConnectButton className="jsx"></ConnectButton>
         </div>
     )
 }
